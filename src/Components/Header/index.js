d33c2c339269f1ce465cb58dcf250f79fa9b5d9e
dhaval-codes <nimisha25dhaval@gmail.com>
2024-01-23 02:16:56 +0530

    made a provision to change profile password partially

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,43 @@
 import React,{useState, useEffect} from 'react'
-import { Heading, MainWrpr } from './index.sc'
+import { ChangePasswordButton, ContainerWrpr, DisplayPictureWrpr, Heading, MainWrpr, NameText, ProfileImage, ProfilePicture, ProfileTooltipWrpr, ProfileWrpr, Samosa } from './index.sc'
 import { ReactComponent as Logo } from '../../Assets/Images/SVGs/header-logo.svg'
 
 export default function ApplicationHeader() {
   const [loginName, setLoginName]=useState('')
+  const [role, setRole]=useState('')
+  const [showProfile, setShowProfile] = useState(false)
+
+  const showFunc = () => {
+    setShowProfile(!showProfile)
+  }
 
   useEffect(()=>{
     setLoginName(window.sessionStorage.getItem('name'))
+    setRole(window.sessionStorage.getItem('role'))
   },[])
 
   return (
     <MainWrpr>
+      <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '1rem'}}>
         <Logo/>
         <Heading>{`Welcome ${loginName}`}</Heading>
+      </div>
+        <ProfileImage src='https://i.redd.it/images-of-lord-ram-and-princess-sita-created-using-v0-ynrenz32csya1.png?width=1024&format=png&auto=webp&s=955ed6cd2f184b16fddd843d787561458aa94387' onClick={showFunc}/>
+        {showProfile && (
+          <ProfileTooltipWrpr>
+            <Samosa/>
+            <ProfileWrpr>
+              <DisplayPictureWrpr>
+                <ProfilePicture src='https://i.redd.it/images-of-lord-ram-and-princess-sita-created-using-v0-ynrenz32csya1.png?width=1024&format=png&auto=webp&s=955ed6cd2f184b16fddd843d787561458aa94387'/>
+              </DisplayPictureWrpr>
+              <ContainerWrpr>
+                <NameText>Name: <span>{loginName}</span></NameText>
+                <NameText>Role: <span>{role}</span></NameText>
+                <ChangePasswordButton>Change Password</ChangePasswordButton>
+              </ContainerWrpr>
+            </ProfileWrpr>
+          </ProfileTooltipWrpr>
+        )}
     </MainWrpr>
   )
 }
--- src/Components/Header/index.sc.js
+++ src/Components/Header/index.sc.js
@@ -5,10 +5,10 @@ export const MainWrpr = styled.div`
     display: flex;
     flex-direction: row;
     gap: 1rem;
-    padding: 1rem;
-    padding-left: 3rem;
+    padding: 1rem 3rem 1rem 3rem;
     background-color: #312B8B;
     align-items: center;
+    justify-content: space-between;
 `;
 
 export const Heading = styled.h1`
@@ -16,4 +16,101 @@ export const Heading = styled.h1`
     font-size: 1.5rem;
     color: #fff;
     cursor: pointer;
+`;
+
+export const ProfileImage = styled.img`
+    height: 3.125rem;
+    width: 3.125rem;
+    border: 1px solid #fff;
+    border-radius: 0.625rem;
+    cursor: pointer;
+`;
+
+export const ProfileTooltipWrpr = styled.div`
+    height: auto;
+    width: auto;
+    position: absolute;
+    right: 3rem;
+    top: 5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    right: 3rem;
+`;
+
+export const Samosa = styled.div`
+    height: 0;
+    width: 0;
+    border-left: 25px solid transparent;
+    border-right: 25px solid transparent;
+    border-bottom: 25px solid #fff;
+
+`;
+
+export const ProfileWrpr = styled.div`
+    height: auto;
+    width: 18.75rem;
+    background-color: #fff;
+    border: none;
+    border-radius: 0.6125rem;
+    box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.25);
+    padding: 0.5rem;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const DisplayPictureWrpr = styled.div`
+    height: 5rem;
+    width: 100%;
+    border-radius: 0.3065rem 0.3065rem 0rem 0rem;
+    background-color: #312B8B;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ProfilePicture = styled.img`
+    height: 6.25rem;
+    width: 6.25rem;
+    border: 3px solid #fff;
+    border-radius: 50%;
+    position: relative;
+    margin-top: 5rem;
+`;
+
+export const ContainerWrpr = styled.div`
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 4rem;
+`;
+
+export const NameText = styled.h2`
+    font-size: 1.125rem;
+    font-weight: 700;
+    margin: 0;
+    text-transform: capitalize;
+    
+    span{
+        font-weight: 400;
+    }
+`;
+
+export const ChangePasswordButton = styled.button`
+    border: none;
+    border-radius: 0.25rem;
+    height: 2.25rem;
+    width: 100%;
+    background-color: #312B8B;
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+    &:hover{
+        background-color: #fff;
+        border: 1px solid #BCBCBC;
+        color: #312B8B;
+        transition: all ease-in-out 0.5s;
+    }
 `;
